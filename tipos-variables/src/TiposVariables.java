bd3d534701b8e13b875afe4aeda572781c3c610e
Santiago Olivera <santiagoolivera176@gmail.com>
2023-06-13 20:19:11 -0300

    tipos variables

--- tipos-variables/src/TiposVariables.java
+++ tipos-variables/src/TiposVariables.java
@@ -1,5 +1,5 @@
-public class App {
+public class TiposVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+                System.out.println("Hello, World!");
     }
 }
